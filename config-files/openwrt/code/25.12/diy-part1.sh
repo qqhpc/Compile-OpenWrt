4ea4aee86093650ff468f079ee9bb299650c2632
hx <hx@whx7.com>
2026-05-02 20:04:43 +0800

    url

--- config-files/openwrt/code/25.12/diy-part1.sh
+++ config-files/openwrt/code/25.12/diy-part1.sh
@@ -3,22 +3,22 @@
 # 描述：OpenWrt DIY 脚本第 1 部分(更新 feed 之前)
 
 # luci-app-passwall
-git clone -q --depth=1 --branch=main --single-branch https://github.com/Openwrt-Passwall/openwrt-passwall.git ./package/luci-app-passwall
+git clone -q --depth=1 --branch=main --single-branch https://github.com/qqhpc/openwrt-passwall.git ./package/luci-app-passwall
 
 # luci-app-passwall2
-git clone -q --depth=1 --branch=main --single-branch https://github.com/Openwrt-Passwall/openwrt-passwall2.git ./package/luci-app-passwall2
+git clone -q --depth=1 --branch=main --single-branch https://github.com/qqhpc/openwrt-passwall2.git ./package/luci-app-passwall2
 
 # luci-app-ssr-plus
-sed -i "/helloworld/d" "feeds.conf.default" && git clone -q --depth=1 --branch=master --single-branch https://github.com/fw876/helloworld.git ./package/luci-app-ssr-plus
+sed -i "/helloworld/d" "feeds.conf.default" && git clone -q --depth=1 --branch=master --single-branch https://github.com/qqhpc/helloworld.git ./package/luci-app-ssr-plus
 
 # passwall-depends
-git clone -q --depth=1 --branch=main --single-branch https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git ./package/passwall-depends
+git clone -q --depth=1 --branch=main --single-branch https://github.com/qqhpc/openwrt-passwall-packages.git ./package/passwall-depends
 
 # luci-app-openclash
-git clone -q --depth=1 --branch=master --single-branch https://github.com/vernesong/OpenClash.git ./package/luci-app-openclash
+git clone -q --depth=1 --branch=master --single-branch https://github.com/qqhpc/OpenClash.git ./package/luci-app-openclash
 
 # luci-app-homeproxy
-git clone -q --depth=1 --branch=master --single-branch https://github.com/immortalwrt/homeproxy.git ./package/luci-app-homeproxy
+git clone -q --depth=1 --branch=master --single-branch https://github.com/qqhpc/homeproxy.git ./package/luci-app-homeproxy
 
 # 更新 go
 
